4c9d7709065cde0e4960d331fe8b65bcfd2e9d6c
atroopa <atroopa@gmail.com>
2024-05-26 06:53:03 -0700

    cleaned robot text

--- wp-content/themes/goolafrooz/front-page.php
+++ wp-content/themes/goolafrooz/front-page.php
@@ -169,10 +169,11 @@
     </div>
   </div>
 
-  <div class="m-1 h-100 md:h-300  w-full">
+  <div class=" m-1 h-100 md:h-300  w-full">
+
   </div>
   <!-- ------------------------------------------------------------------- -->
-  <section class=" relative flex h-[100vh] md:h-[500px] w-full isolate items-center justify-center overflow-hidden py-5 md:py-10 video-container">
+  <section class=" bg-red-100 relative flex h-[50vh] md:h-[500px] w-full isolate items-center justify-center overflow-hidden py-5 md:py-10 video-container">
     <video src="<?php echo get_template_directory_uri().'/img/video/robot-4.mp4' ?>" autoplay loop playsinline muted class="absolute left-0 top-0 z-0 h-[50vh] md:h-full w-full object-cover"></video>
     <div class="absolute inset-0 z-10 backdrop-blur bg-gray/30  p-5 rounded-2xl h-[50vh] md:h-full"></div>
     <div class="z-20 text-right">
@@ -188,7 +189,7 @@
           style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
         </div>
       </div>
-      <div class="mx-auto max-w-7xl px-6 lg:px-8">
+      <!-- <div class="mx-auto max-w-7xl px-6 lg:px-8">
         <div class="mx-auto max-w-2xl lg:mx-0">
           <h2 class="text-sm md:text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">شرکت گلخانه سازی گل افروز</h2>
           <p class="mt-6 text-sm md:text-lg leading-8 text-gray-900 font-bold">
@@ -196,7 +197,7 @@
           </p>
         </div>
         <div class="mx-auto mt-2 max-w-2xl lg:mx-0 lg:max-w-none">
-          <div class="text-sm md:text-xl grid grid-cols-1 gap-x-8 gap-y-6 text-base font-semibold leading-7 font-bold text-gray-900 sm:grid-cols-2 md:flex lg:gap-x-10">
+          <div class="text-sm md:text-xl grid grid-cols-1 gap-x-8 gap-y-7 text-base font-semibold leading-7 font-bold text-gray-900 sm:grid-cols-2 md:flex lg:gap-x-10">
             <a href="#"><span class="p-1">⇚</span>قبل از هرچیزی بخوانید </a>
             <a href="#"><span class="p-1">⇚</span>نکات مهم گلخانه سازی</a>
             <a href="#"><span class="p-1">⇚</span>چرا مشاوره مهم است</a>
@@ -219,7 +220,7 @@
               <dt class="hidden md:text-base leading-7 text-gray-900 font-bold">انتخاب همیشه شما خواهیم شد</dt>
               <dd class="hidden md:text-2xl font-bold leading-9 tracking-tight text-900">مسئول</dd>
             </div>
-          </dl>
+          </dl> -->
         </div>
       </div>
     </div>
