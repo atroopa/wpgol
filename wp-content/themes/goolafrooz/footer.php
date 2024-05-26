4525e91cd8135c90335c35f0eb1305e88fbc7454
atroopa <atroopa@gmail.com>
2024-05-26 04:37:02 -0700

    footer changed

--- wp-content/themes/goolafrooz/footer.php
+++ wp-content/themes/goolafrooz/footer.php
@@ -2,7 +2,7 @@
 
 
 <footer class="relative  text-center font-bold text-brown lg:text-left ">
-  <div class="bg-cover bg-center w-full h-full " style="background-image: url(<?php echo get_template_directory_uri().'/img/footer/footer-1.png' ?>)">
+  <div class="bg-cover bg-center w-full h-max-screen md:h-full " style="background-image: url(<?php echo get_template_directory_uri().'/img/footer/footer-1.png' ?>)">
     <div class="flex items-center justify-center  py-20"></div>
     <div class="flex items-center justify-center border-b-2 border-neutral-200 p-6 dark:border-white/10 lg:justify-between">
       <div class="me-12 hidden lg:block backdrop-blur-2xl bg-white/30  p-5 rounded-2xl">
@@ -137,5 +137,4 @@
   </div>
 </footer>
 </body>
-
 </html>
