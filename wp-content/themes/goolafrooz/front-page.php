35c5435c49a036916ad03c5eee6b8aa49b41d905
atroopa <atroopa@gmail.com>
2024-05-26 22:39:58 -0700

    clean video text

--- wp-content/themes/goolafrooz/front-page.php
+++ wp-content/themes/goolafrooz/front-page.php
@@ -174,8 +174,8 @@
   </div>
   <!-- ------------------------------------------------------------------- -->
   <section class=" bg-red-100 relative flex h-[50vh] md:h-[500px] w-full isolate items-center justify-center overflow-hidden py-5 md:py-10 video-container">
-    <video src="<?php echo get_template_directory_uri().'/img/video/robot-4.mp4' ?>" autoplay loop playsinline muted class="absolute left-0 top-0 z-0 h-[50vh] md:h-full w-full object-cover"></video>
-    <div class="absolute inset-0 z-10 backdrop-blur bg-gray/30  p-5 rounded-2xl h-[50vh] md:h-full"></div>
+    <video src="<?php echo get_template_directory_uri().'/img/video/robot-5.mp4' ?>" autoplay loop playsinline muted class="absolute left-0 top-0 z-0 h-[50vh] md:h-full w-full object-cover"></video>
+    <!-- <div class="absolute inset-0 z-10 backdrop-blur bg-gray/30  p-5 rounded-2xl h-[50vh] md:h-full"></div>
     <div class="z-20 text-right">
       <div class="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
         aria-hidden="true">
@@ -188,7 +188,7 @@
         <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
           style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
         </div>
-      </div>
+      </div> -->
       <!-- <div class="mx-auto max-w-7xl px-6 lg:px-8">
         <div class="mx-auto max-w-2xl lg:mx-0">
           <h2 class="text-sm md:text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">شرکت گلخانه سازی گل افروز</h2>
