1b17ef81343821626730960584117b8c9aff6610
atroopa <atroopa@gmail.com>
2024-05-27 00:20:39 -0700

    footer finish

--- wp-content/themes/goolafrooz/footer.php
+++ wp-content/themes/goolafrooz/footer.php
@@ -2,16 +2,17 @@
 
 
 <footer class="relative  text-center font-bold text-brown lg:text-left ">
-  <div class="bg-cover bg-center w-full h-max-screen md:h-full " style="background-image: url(<?php echo get_template_directory_uri().'/img/footer/footer-1.png' ?>)">
+  <div class="bg-contain  bg-no-repeat md:bg-cover bg-center w-full h-max-screen md:h-full "
+  style="background-image: url(<?php echo get_template_directory_uri().'/img/footer/footer-1.png' ?>)">
     <div class="flex items-center justify-center  py-20"></div>
     <div class="flex items-center justify-center border-b-2 border-neutral-200 p-6 dark:border-white/10 lg:justify-between">
       <div class="me-12 hidden lg:block backdrop-blur-2xl bg-white/30  p-5 rounded-2xl">
-        <h1 class="bg-gradient-to-r from-red-600 via-blue-600 to-green-600 inline-block text-transparent bg-clip-text">
+        <h1 class="hidden md:block bg-gradient-to-r from-red-600 via-blue-600 to-green-600 inline-block text-transparent bg-clip-text">
         پل ارتباطی با شرگت گلخانه سازی گل افروز
         </h1>
       </div>
       <!-- Social network icons container -->
-      <div class="flex justify-center backdrop-blur-2xl bg-white/30  p-5 rounded-2xl">
+      <div class="flex items-center justify-center backdrop-blur-sm bg-white/1 px-3 py-2  md:p-5 rounded-2xl">
         <a href="https://api.whatsapp.com/send?phone=+989147281416" class="me-6 [&>svg]:h-8 [&>svg]:w-8">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#21A852" class="bi bi-whatsapp" viewBox="0 0 16 16">
             <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232" />
@@ -35,11 +36,11 @@
     </div>
 
     <!-- Main container div: holds the entire content of the footer, including four sections (TW Elements, Products, Useful links, and Contact), with responsive styling and appropriate padding/margins. -->
-    <div class="mx-0 md:mx-6 py-5 text-center md:text-left backdrop-blur-lg bg-white/30  p-5 rounded-2xl">
+    <div class="mx-0 md:mx-6 py-9 text-center md:text-left backdrop-blur-lg bg-white/30  p-5 rounded-2xl">
       <div class="grid-1 grid gap-8 md:grid-cols-2 lg:grid-cols-4">
         <!-- Contact section -->
         <div>
-          <h6 class="mb-4 flex justify-center font-semibold uppercase md:justify-start">
+          <h6 class="mb-4  flex justify-center font-semibold uppercase md:justify-start">
             ارتباط با ما
           </h6>
           <p class="mb-4 flex text-right items-center justify-center md:justify-start">
@@ -50,7 +51,7 @@
               </svg>
             </span>
 
-            البرز - کرج - عظیمیه بالاتر از پل آزادگان - برج قائم
+             استان البرز - کرج - محل عظیمیه - طالقانی شمالی
 
           </p>
           <p class="mb-4 flex items-center justify-center md:justify-start">
@@ -121,8 +122,8 @@
           </p>
         </div>
       </div>
-      <div class=" hidden md:block flex flex-col items-center justify-center">
-      <div class=" mb-4 flex flex-col items-start justify-start font-semibold uppercase md:justify-center">
+      <div class=" flex flex-col items-center justify-center hidden md:block ">
+      <div class="  mb-4 flex flex-col items-center justify-center font-semibold uppercase md:justify-center">
           <img src="<?php echo get_template_directory_uri().'/img/slider//icons/enamad.png' ?>" alt="Your Logo" class="h-20 w-20" />
         </div>
         <div class="py-1">
